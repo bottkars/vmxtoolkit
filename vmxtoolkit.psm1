364ec57269ed7fb415c8a5dac2e3965041e7db5c
Karsten Bott <Karsten.Bott@bottnet.de>
2015-08-06 06:56:59 +0200

    added function set-vmxmainmemory to vmxtoolkit

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -1980,7 +1980,7 @@ function New-VMXClone
 			{
 			
 			# $VerbosePreference = [System.Management.Automation.ActionPreference]::Continue
-				Write-Verbose "Creating Linked Clone  $Clonename for $Basesnapshot in $Cloneconfig"
+				Write-Verbose "Creating Full Clone  $Clonename for $Basesnapshot in $Cloneconfig"
 			($cmdresult = &$vmrun clone $config $Cloneconfig full $BaseSnapshot $Clonename) #  2>&1 | Out-Null
 			#  &$vmrun clone $MasterVMX $CloneVMX linked Base
 				#write-log "$origin snapshot  $cmdresult"
@@ -3664,6 +3664,60 @@ function Get-VMXSnapshotconfig
 		
 	}
 }
+
+
+
+#mainMem.useNamedFile = "FALSE"#
+<#
+	.DESCRIPTION
+		Sets the Memory in MB for a VM
+#>
+function Set-VMXMainMemory {
+	[CmdletBinding(DefaultParametersetName = "2",HelpUri = "http://labbuildr.bottnet.de/modules/set-VMXMEMFile/")]
+	param (
+	[Parameter(ParameterSetName = "2", Mandatory = $false, ValueFromPipelineByPropertyName = $True)][Alias('NAME','CloneName')]$VMXName,
+	[Parameter(ParameterSetName = "2", Mandatory = $false, ValueFromPipelineByPropertyName = $True)]$config,
+	[Parameter(ParameterSetName = "2", Mandatory = $true, ValueFromPipelineByPropertyName = $True)][switch]$usefile
+	)
+	begin
+	{
+	}
+	process
+	{
+		switch ($PsCmdlet.ParameterSetName)
+		{
+			"1"
+			{ $vmxconfig = Get-VMXConfig -VMXName $VMXname }
+			"2"
+			{ $vmxconfig = Get-VMXConfig -config $config }
+		}
+        if ((get-vmx -Path $config).state -eq "stopped" )
+        {
+        Write-Verbose "We got to set mainMem.useNamedFile to $($usefile.IsPresent) "
+        $vmxconfig = $vmxconfig | where {$_ -NotMatch "mainMem.useNamedFile"}
+        If ($usefile.IsPresent)
+            {
+            $vmxconfig += 'mainMem.useNamedFile = "TRUE"'
+            }
+        else
+            {
+            $vmxconfig += 'mainMem.useNamedFile = "FALSE"'
+            }
+
+        $vmxconfig | Set-Content -Path $config
+		$object = New-Object -TypeName psobject
+		$Object | Add-Member -MemberType NoteProperty -Name VMXName -Value $VMXName
+		$object | Add-Member -MemberType NoteProperty -Name UseMemFile -Value $($usefile.IsPresent)
+		Write-Output $Object
+        }
+        else
+        {
+        Write-Warning "VM must be in stopped state"
+        }
+	}
+	end { }
+} 
+
 
 <#
 	.DESCRIPTION
