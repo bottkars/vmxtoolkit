e317407ca73fae2aa1228a2c0e25ebb8942f3393
Karsten Bott <Karsten.Bott@bottnet.de>
2015-09-04 10:33:27 +0200

    Changed ISO Handling

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -2838,8 +2838,10 @@ function Connect-VMXcdromImage
 	(
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)][Alias('NAME','CloneName')][string]$VMXName,
 		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]$config,
-		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]$ISOfile,
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)][ValidateSet('IDE','SATA')]$Contoller = 'SATA'
+		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]$ISOfile,
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)][ValidateSet('ide','sata')]$Contoller = 'sata',
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)][ValidateSet('0:0','0:1','1:0','1:1')]$Port = '0:1',
+        [Parameter(Mandatory = $false)][switch]$connect=$true
 	)
 	
 	Begin
@@ -2851,27 +2853,38 @@ function Connect-VMXcdromImage
         if ((get-vmx -Path $config).state -eq "stopped")
         {
 		$Content = Get-Content -Path $config
-		Write-verbose "Checking for $($Contoller)0.present"
-		if (!($Content -match "$($Contoller)0.present"))
+		Write-verbose "Checking for $($Contoller)$($Port).present"
+		if (!($Content -notmatch " $($Contoller)$($Port).present"))
              {
-             Write-Warning "Controller $($Contoller)0 not present"
+             Write-Warning "Controller $($Contoller)$($Port) not present"
              }
         else
             {
-            $Content = $Content -notmatch "sata0:1"
-            $Content += 'sata0:1.present = "TRUE"'
-            $Content += 'sata0:1.autodetect = "TRUE"'
-            $Content += 'sata0:1.deviceType = "cdrom-image"'
-            $Content += 'sata0:1.startConnected = "TRUE"'
-            $Content += 'sata0:1.fileName = "'+$ISOfile+'"'
+            $object = New-Object -TypeName psobject		    
+            $Object | Add-Member -MemberType NoteProperty -Name VMXName -Value $VMXName
+	        $object | Add-Member -MemberType 'NoteProperty' -Name Config -Value $config
+            $object | Add-Member -MemberType NoteProperty -Name Controller -Value "$($Contoller)$($Port)"
+            $Content = $Content -notmatch "$($Contoller)$($Port)"
+            $Content += "$($Contoller)$($Port)"+'.present = "TRUE"'
+            $Content += "$($Contoller)$($Port)"+'.autodetect = "TRUE"'
+            If ($connect.IsPresent -and $ISOfile)
+                {
+                $Content += "$($Contoller)$($Port)"+'.deviceType = "cdrom-image"'
+                $Content += "$($Contoller)$($Port)"+'.startConnected = "TRUE"'
+                $Content += "$($Contoller)$($Port)"+'.fileName = "'+$ISOfile+'"'
+                $object | Add-Member -MemberType NoteProperty -Name ISO -Value $ISOfile
+                
+                }
+            else
+                {
+                $Content += "$($Contoller)$($Port)"+'.deviceType = "cdrom-raw"'
+                $Content += "$($Contoller)$($Port)"+'.startConnected = "FALSE"'
+                }
             $Content | Set-Content -Path $config
-            $object = New-Object -TypeName psobject
-		    $Object | Add-Member -MemberType NoteProperty -Name VMXName -Value $VMXName
-		    $object | Add-Member -MemberType 'NoteProperty' -Name Config -Value $config
-		    # $object | Add-Member -MemberType 'NoteProperty' -Name Path -Value $Path
-		    $object | Add-Member -MemberType NoteProperty -Name Controller -Value "$($Contoller)0"
-		    $object | Add-Member -MemberType NoteProperty -Name ISO -Value $ISOfile
+	        $object | Add-Member -MemberType 'NoteProperty' -Name Connected -Value $connect
             Write-Output $object
+
+
             }
         }
         
