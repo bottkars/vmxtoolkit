e7656769fff25e8da5761b1f787a4af95d9b1119
Karsten Bott <Karsten.Bott@emc.com>
2016-02-29 17:18:35 +0100

    added client2host copy

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -4383,7 +4383,7 @@ function convert-VMXdos2unix
 
 
 
-function copy-VMXfile2guest
+function Copy-VMXFile2Guest
 {
 	param (
 	[Parameter(ParameterSetName = "2", Mandatory = $false, ValueFromPipelineByPropertyName = $True)][Alias('NAME','CloneName')]$VMXName,
@@ -4422,6 +4422,47 @@ function copy-VMXfile2guest
 	{
 	}
 }
+
+function Copy-VMXFile2Host
+{
+	param (
+	[Parameter(ParameterSetName = "2", Mandatory = $false, ValueFromPipelineByPropertyName = $True)][Alias('NAME','CloneName')]$VMXName,
+	[Parameter(ParameterSetName = "2", Mandatory = $true, ValueFromPipelineByPropertyName = $True)]$config,
+    [Parameter(ParameterSetName = "2", Mandatory = $true)]$Sourcefile,
+    [Parameter(ParameterSetName = "2", Mandatory = $true)]$targetfile,
+    [Parameter(ParameterSetName = 2, Mandatory = $false, ValueFromPipelineByPropertyName = $true)][Alias('gu')]$Guestuser, 
+    [Parameter(ParameterSetName = 2, Mandatory = $false, ValueFromPipelineByPropertyName = $true)][Alias('gp')]$Guestpassword
+    )
+    begin
+	{
+	}
+	process
+	{
+	do
+	    {
+		($cmdresult = &$vmrun -gu $Guestuser -gp $Guestpassword copyfilefromguesttohost $config $Sourcefile "$targetfile")  2>&1 | Out-Null
+	    write-verbose "$cmdresult"
+		}
+		until ($VMrunErrorCondition -notcontains $cmdresult)
+        if ($LASTEXITCODE -eq 0)
+        {
+        $object = New-Object -TypeName psobject
+        $Object | Add-Member -MemberType NoteProperty -Name VMXName -Value $VMXName
+	    $object | Add-Member -MemberType NoteProperty -Name Sourcefile -Value $sourcefile
+        $object | Add-Member -MemberType NoteProperty -Name Direction -Value "==>"
+	    $object | Add-Member -MemberType NoteProperty -Name Target -Value $targetfile
+        $object | Add-Member -MemberType NoteProperty -Name CreateTime -Value (Get-Date -Format "yyyy.MM.dd hh:mm")
+        Write-Output $object
+        }
+        else 
+            {Write-Warning $cmdresult}
+        
+	}
+    end
+	{
+	}
+}
+
 
 function Copy-VMXDirHost2Guest
 {
