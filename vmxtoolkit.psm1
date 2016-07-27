4add953935fb7d9f5742bb01c757163cf0fa7977
Karsten Bott <Karsten.Bott@bottnet.de>
2016-07-27 21:34:26 +0200

    added set-vmxuuid to tweax unity n:-)

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -1524,6 +1524,47 @@ function Set-VMXDisplayName
 	}
 }
 
+function Set-VMXUUID
+{
+	[CmdletBinding(HelpUri = "http://labbuildr.bottnet.de/modules/Set-VMXUUID")]
+	param
+	(
+		[Parameter(Mandatory = $false,
+				   ValueFromPipelineByPropertyName = $true,
+				   HelpMessage = 'Please Specify Valid Config File')]$config,
+		[Parameter(Mandatory = $false,
+				   ValueFromPipelineByPropertyName = $False,
+				   HelpMessage = 'Please Specify New Value for UUID')] [ValidatePattern('^[0-9a-f]{16}-[0-9a-f]{16}$')][Alias('Value')]$UUID
+	)
+	
+	Begin
+	{
+		
+		
+	}
+	Process
+	{
+        if ((get-vmx -Path $config).state -eq "stopped")
+        {
+		$Content = Get-Content $config | where { $_ -ne "" }
+		$Content = $content | where { $_ -NotMatch "uuid.bios" }
+		$content += 'uuid.bios = "' + $UUID + '"'
+		Set-Content -Path $config -Value $content -Force
+		$object = New-Object -TypeName psobject
+		$Object | Add-Member -MemberType NoteProperty -Name Config -Value $config
+		$object | Add-Member -MemberType NoteProperty -Name UUID -Value $UUID
+		Write-Output $Object
+        }
+		else
+        {
+        Write-Warning "VM must be in stopped state"
+        }
+	}
+	End
+	{
+		
+	}
+}
 
 <#
 	.SYNOPSIS
