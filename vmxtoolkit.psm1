df3e0e98c8d4c1c651fea7488dda39a2f75433a7
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-22 18:28:25 +0200

    sync

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -2415,8 +2415,9 @@ function New-VMXLinkedClone
 		Write-Host -ForegroundColor Magenta $Clonename -NoNewline
 		do
 			{
-			$snapcommand = "$vmrun clone $config $Cloneconfig linked -snapShot=$($BaseSnapshot) -cloneNme=$($Clonename)" # 2>&1 | Out-Null
+			$snapcommand = "$vmrun clone $config $Cloneconfig linked -snapshot=$($BaseSnapshot) -cloneName=$($Clonename)" # 2>&1 | Out-Null
 			Write-Host $snapcommand
+			$cmdresult = .$snapcommand
 			$cmdresult
 			}
 		until ($VMrunErrorCondition -notcontains $cmdresult)
