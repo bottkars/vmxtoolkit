096c72692a21543e774f69da37a89bf71de3c241
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-25 01:16:47 +0200

    fileexists

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -5408,3 +5408,31 @@ tools.remindInstall = "FALSE"')
     $object | Add-Member -MemberType NoteProperty -Name Path -Value $VMXpath
     Write-Output $object
 }
+
+
+
+function Test-VMXFileInGuest
+{
+	[CmdletBinding(HelpUri = "https://github.com/bottkars/vmxtoolkit/wiki")]
+	param (
+	[Parameter(ParameterSetName = "1", Mandatory = $false, ValueFromPipelineByPropertyName = $True)]$Filename,
+	[Parameter(ParameterSetName = "1", Mandatory = $false, ValueFromPipelineByPropertyName = $True)][Alias('NAME','CloneName')]$VMXName,
+	[Parameter(ParameterSetName = "1", Mandatory = $true, ValueFromPipelineByPropertyName = $True)]$config,
+    [Parameter(ParameterSetName = 1, Mandatory = $true, ValueFromPipelineByPropertyName = $true)][Alias('gu')]$Guestuser, 
+    [Parameter(ParameterSetName = 1, Mandatory = $true, ValueFromPipelineByPropertyName = $true)][Alias('gp')]$Guestpassword
+    )
+begin {
+    }
+process 
+	{
+    $fileok = .$vmrun -gu $Guestuser -gp $Guestpassword fileExistsInGuest  $config $Filename
+	if ($fileok -match "exists")
+	 {Write-Output $True}
+	 else
+	 {Write-Output $false}
+    }
+end 
+	{
+    
+    }
+}
