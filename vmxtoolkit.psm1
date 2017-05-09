4f0ca1412899f2cd02261559af14b96f3f38f4df
bottkars <Karsten.Bott@bottnet.de>
2017-05-09 13:21:20 +0200

    expand vmxdisk

--- vmxtoolkit.psm1
+++ vmxtoolkit.psm1
@@ -406,6 +406,56 @@ function Get-VMXConfigVersion
 
 <#	
 	.SYNOPSIS
+	Expand-VMXDiskfile
+	
+	.DESCRIPTION
+		Shrinks the VMS Disk File
+	
+	.EXAMPLE
+		get-vmx test | get-vmxscsidisk | Resize-VMXDiskfile
+	
+	.NOTES
+		requires VMXtoolkit loaded
+#>
+function Expand-VMXDiskfile
+{
+	[CmdletBinding(DefaultParametersetName = "1",HelpUri = "http://labbuildr.bottnet.de/modules")]
+	param (
+
+	[Parameter(ParameterSetName = "1", Mandatory = $True, ValueFromPipelineByPropertyName = $True)]$DiskPath,
+    [Parameter(ParameterSetName = "1", Mandatory = $True, ValueFromPipelineByPropertyName = $True)]$Disk,
+	[Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)][uint64]$NewSize,
+    [Parameter(ParameterSetName = "2", Mandatory = $True, ValueFromPipelineByPropertyName = $false)]$Diskfile
+
+	)
+	begin
+	{
+	}
+	process
+	{
+		switch ($PsCmdlet.ParameterSetName)
+		{
+			"1"
+			{
+            $Diskfile = Join-Path $DiskPath $Disk
+            }
+            default
+            {}
+
+		}
+        Write-Warning "Expanding $Diskfile"
+	    & $Global:vmware_vdiskmanager -x $NewSize $Diskfile
+        Write-Verbose "Exitcode: $LASTEXITCODE"
+		
+	}
+	end { }
+}
+
+
+
+
+<#	
+	.SYNOPSIS
 	Resize-VMXDiskfile
 	
 	.DESCRIPTION
