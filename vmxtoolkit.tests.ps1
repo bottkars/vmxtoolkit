47c293a4491f2e3c8d2a6e1fd5337257b35c66f1
bottkars <Karsten.Bott@emc.com>
2017-07-03 12:09:03 +0200

    test

--- vmxtoolkit.tests.ps1
+++ vmxtoolkit.tests.ps1
@@ -1,4 +1,4 @@
-#.
+#
 # This is a PowerShell Unit Test file.
 # You need a unit test framework such as Pester to run PowerShell Unit tests. 
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
